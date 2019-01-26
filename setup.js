83714340a1edcba4d6699fec01c032ff8f992898
jagilley <37783831+jagilley@users.noreply.github.com>
2019-01-25 22:51:54 -0600

    unbuffered python scripts

--- setup.js
+++ setup.js
@@ -1,11 +1,16 @@
 const { spawn } = require('child_process');
-const process = spawn("python3", ["/Users/jaspergilley/Code/pylectron/setup.py"])
-
+const process = spawn("python3", ["-u", "/Users/jaspergilley/Code/pylectron/setup.py"])
+/*
 process.stdout.on('data', (data) => {
     outputText = `stdout: ${data}`;
     console.log(outputText);
     document.getElementById("outElement").innerHTML = outputText;
-  });
+  });*/
+
+process.stdout.on('data', function(data) {
+    console.log(data.toString());
+    document.getElementById("outElement").innerHTML = document.getElementById("outElement").innerHTML + data.toString() + "<br>";
+});
   
 process.stderr.on('data', (data) => {
     console.log(`stderr: ${data}`);
